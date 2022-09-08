96503635fe14b32e80110e1ca8911efaa2947fbd
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-09-08 16:48:36 -0400

    added NavLinks to welcome and products pages, plus active styling

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome/>
-      </Route>
-      <Route path="/products">
-        <Products/>
-      </Route>
+      <MainHeader/>
+      <main>
+        <Route path="/welcome">
+          <Welcome/>
+        </Route>
+        <Route path="/products">
+          <Products/>
+        </Route>
+      </main>
     </div>
   );
 }
